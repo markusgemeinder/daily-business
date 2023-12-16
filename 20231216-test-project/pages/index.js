fa7e880df23444297e93d4823a8e94eec2ade504
markusgemeinder <190774@gmx.de>
2023-12-16 20:08:11 +0100

    Update

---
+++ 20231216-test-project/pages/index.js
@@ -0,0 +1,12 @@
+import Link from "../components/Link";
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>
+        <Link href="/">Home</Link>
+      </p>
+    </div>
+  );
+}
